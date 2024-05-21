af30f7bfee14af94a524436757758798c6aa758d
GiordanoGuidi <giordanoguidi23@gmail.com>
2024-05-21 18:27:52 +0200

    refactoring class Main

--- src/org/learning/Main.java
+++ src/org/learning/Main.java
@@ -6,56 +6,90 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        //Variabile per la lunghezza dell'array
-        System.out.println("quanti libri sono?");
-        //Assegno il valore alla variuabile
-        int arrayLength = Integer.parseInt(scanner.nextLine());
-        try {
-            if (arrayLength <= 0){
-                throw new IllegalArgumentException("Deve essere un numero maggiore di 0");
+        int arrayLength;
+        //Continuo a chiedere fino a quando non viene dato un valore corretto
+        while (true){
+            try {
+            //Variabile per la lunghezza dell'array
+            System.out.println("quanti libri sono?");
+            //Assegno il valore alla variuabile
+            arrayLength = Integer.parseInt(scanner.nextLine());
+                if (arrayLength <= 0){
+                    throw new IllegalArgumentException("Deve essere un numero maggiore di 0");
+                }
+                //Se il valore è corretto esco dal ciclo
+                break;
+            } catch (IllegalArgumentException e) {
+                System.out.println(e.getMessage());
             }
-        } catch (IllegalArgumentException e) {
-            System.out.println(e.getMessage());
         }
         //Definisco la lunghezza dell'array
         Book[] books = new Book[arrayLength];
+        //Faccio un ciclo che itera sulla lunghezza dell'array books
         for (int i = 0; i < books.length;i++){
-            System.out.println("Qual'è il titolo del libro?");
-            String title = (scanner.nextLine());
-            try {
-                if (title.trim().isEmpty()){
-                    throw new IllegalArgumentException("Il campo non può essere vuoto");
+            String title;
+            //Continuo a chiedere fino a quando non viene dato un valore corretto
+            while(true){
+                System.out.println("Qual'è il titolo del libro?");
+                title = (scanner.nextLine());
+                try {
+                    if (title.trim().isEmpty()){
+                        throw new IllegalArgumentException("Il campo non può essere vuoto");
+                    }
+                    //Se il valore è corretto esco dal ciclo
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getMessage());
                 }
-                System.out.println("Il titolo del libro è: " + title);
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getMessage());
             }
-            System.out.println("Quate pagina ha?");
-            int pages = Integer.parseInt(scanner.nextLine());
-            try {
-                if (pages <= 0){
-                   throw new IllegalArgumentException("Il numero di pagine deve essere un numero maggiore di 0");
+            int pages;
+            //Continuo a chiedere fino a quando non viene dato un valore corretto
+            while(true){
+                System.out.println("Quate pagina ha?");
+                pages = Integer.parseInt(scanner.nextLine());
+                try {
+                    if (pages <= 0){
+                       throw new IllegalArgumentException("Il numero di pagine deve essere un numero maggiore di 0");
+                    }
+                    //Se il valore è corretto esco dal ciclo
+                    break;
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
+                //Catturo prima la sottoclasse NumberFormatException
+                catch (NumberFormatException e) {
+                    System.out.println("Inserisci un numero valido");
+                } catch (IllegalArgumentException e){
+                    System.out.println(e.getMessage());
+                }
             }
-            System.out.println("Chi è l'autore");
-            String author = (scanner.nextLine());
-            try {
-                if (author.trim().isEmpty()){
-                    throw new IllegalArgumentException("Il campo non può essere vuoto");
+            String author;
+            //Continuo a chiedere fino a quando non viene dato un valore corretto
+            while(true){
+                System.out.println("Chi è l'autore");
+                author = (scanner.nextLine());
+                try {
+                    if (author.trim().isEmpty()){
+                        throw new IllegalArgumentException("Il campo non può essere vuoto");
+                    }
+                    //Se il valore è corretto esco dal ciclo
+                    break;
+                } catch (IllegalArgumentException e) {
+                    System.out.println(e.getClass());
                 }
-            } catch (IllegalArgumentException e) {
-                System.out.println(e.getClass());
             }
-            System.out.println("Chi è l'editore");
-            String publisher = (scanner.nextLine());
-            try {
-                if (publisher.trim().isEmpty()){
-                    throw new IllegalArgumentException("Il campo non può essere vuoto");
+            String publisher;
+            //Continuo a chiedere fino a quando non viene dato un valore corretto
+            while(true){
+                System.out.println("Chi è l'editore");
+                publisher = (scanner.nextLine());
+                try {
+                    if (publisher.trim().isEmpty()){
+                        throw new IllegalArgumentException("Il campo non può essere vuoto");
+                    }
+                    //Se il valore è corretto esco dal ciclo
+                    break;
+                } catch (Exception e) {
+                    System.out.println(e.getMessage());
                 }
-            } catch (Exception e) {
-                System.out.println(e.getMessage());
             }
             books[i]= new Book(title,pages,author,publisher);
             books[i].setPages(0);
